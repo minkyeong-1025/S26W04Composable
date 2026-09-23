cf105b871dd3d316147b6dbb23ea2e0535a59fb7
minkyeong-1025 <GitHub dongdongus7@gmail.com>
2026-09-23 16:41:26 +0900

    🎉Project 시작

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20241187/s26w04composable/MainActivity.kt
@@ -0,0 +1,36 @@
+package kr.ac.kumoh.ce.s20241187.s26w04composable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kr.ac.kumoh.ce.s20241187.s26w04composable.ui.theme.S26W04ComposableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            S26W04ComposableTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
+}
